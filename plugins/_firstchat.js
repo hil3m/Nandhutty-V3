cbdc082311074ffddd866cda0b71ea1fbf2db15e
hil3m <67930632+hil3m@users.noreply.github.com>
2022-04-02 18:47:42 +0300

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -9,7 +9,7 @@ handler.all = async function (m) {
 Hai, ${this.ucapan()}
 
 ${user.banned ? 'kamu dibanned' : `Ada yang bisa ${this.user.name} bantu?`}
-`.trim(), 'ɴᴀɴᴅʜᴜᴛᴛʏ ᴠ3', user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : ',?', m)
+`.trim(), 'HIL3M', user.banned ? 'Sahip' : 'Menu', user.banned ? ',owner' : ',?', m)
     user.pc = new Date * 1
 }
 
